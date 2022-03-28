bd2a03107600c43fcaf29c75688e109fd8f3702a
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2022-03-28 23:56:10 +0700

    done design navbar components

---
+++ components/navbar/navbar.js
@@ -0,0 +1,135 @@
+import Image from "next/image";
+import styled from "styled-components";
+import Link from "next/link";
+import logo from "../../public/assets/logo.svg";
+import home from "../../public/assets/icon-nav-home.svg";
+import homeActive from "../../public/assets/icon-nav-home-active.svg";
+import film from "../../public/assets/icon-nav-movies.svg";
+import filmActive from "../../public/assets/icon-nav-movies-active.svg";
+import tv from "../../public/assets/icon-nav-tv-series.svg";
+import tvActive from "../../public/assets/icon-nav-tv-series-active.svg";
+import bookmark from "../../public/assets/icon-nav-bookmark.svg";
+import bookmarkActive from "../../public/assets/icon-nav-bookmark-active.svg";
+import { useRouter } from "next/router";
+
+const Navbar = () => {
+  const router = useRouter();
+
+  return (
+    <Wrap>
+      <div className="first">
+        <div className="logo">
+          <Image src={logo} alt="Logo" w />
+        </div>
+      </div>
+      <div className="menu">
+        <ul>
+          <li>
+            {router.pathname === "/" ? (
+              <Link scroll={true} href="/" passHref>
+                <a>
+                  <Image src={homeActive} alt="Home" />
+                </a>
+              </Link>
+            ) : (
+              <Link scroll={true} href="/" passHref>
+                <a>
+                  <Image src={home} alt="Home" />
+                </a>
+              </Link>
+            )}
+          </li>
+          <li>
+            {router.pathname === "/movies" ? (
+              <Link scroll={true} href="/movies" passHref>
+                <a>
+                  <Image src={filmActive} alt="Home" />
+                </a>
+              </Link>
+            ) : (
+              <Link scroll={true} href="/movies" passHref>
+                <a>
+                  <Image src={film} alt="Home" />
+                </a>
+              </Link>
+            )}
+          </li>
+          <li>
+            {router.pathname === "/tv" ? (
+              <Link scroll={true} href="/tv" passHref>
+                <a>
+                  <Image src={tvActive} alt="Home" />
+                </a>
+              </Link>
+            ) : (
+              <Link scroll={true} href="/tv" passHref>
+                <a>
+                  <Image src={tv} alt="Home" />
+                </a>
+              </Link>
+            )}
+          </li>
+          <li>
+            {router.pathname === "/bookmark" ? (
+              <Link scroll={true} href="/bookmark" passHref>
+                <a>
+                  <Image src={bookmarkActive} alt="Home" />
+                </a>
+              </Link>
+            ) : (
+              <Link scroll={true} href="/bookmark" passHref>
+                <a>
+                  <Image src={bookmark} alt="Home" />
+                </a>
+              </Link>
+            )}
+          </li>
+        </ul>
+      </div>
+      <div className="second">
+        <div className="avatar"></div>
+      </div>
+    </Wrap>
+  );
+};
+
+const Wrap = styled.nav`
+  background-color: var(--color-semi-Semi-dark);
+  padding: 1.8rem 1.6rem;
+  display: flex;
+  justify-content: space-between;
+  .first {
+    display: flex;
+    align-items: center;
+  }
+  .menu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    ul {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      list-style: none;
+      li:first-child {
+        margin: 0 1rem 0 0;
+      }
+      li {
+        margin: 0 1rem;
+      }
+    }
+  }
+  .second {
+    display: flex;
+    justify-content: flex-end;
+    .avatar {
+      justify-self: flex-end;
+      width: 40px;
+      height: 40px;
+      background-color: var(--color-dark-blue);
+      border-radius: 50%;
+    }
+  }
+`;
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
+import styled from "styled-components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/assets/logo.svg" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+
+        <meta name="theme-color" content="#FC4747" />
+      </Head>
+      <Wrap>
+        <Component {...pageProps} />
+      </Wrap>
+    </>
+  );
 }
 
-export default MyApp
+const Wrap = styled.div`
+  /* padding: 0 1.6rem; */
+`;
+
+export default MyApp;
