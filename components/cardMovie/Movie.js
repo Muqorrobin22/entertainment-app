6c2c448e45a23a59c3f94f83b067d424ec9380dc
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2022-03-30 00:27:11 +0700

    modularizing card moviess into components in components/cardMovies/movie.js

---
+++ components/cardMovie/Movie.js
@@ -0,0 +1,132 @@
+import Image from "next/image";
+import styled from "styled-components";
+import data from "../../public/data.json";
+import bookmark from "../../public/assets/Bookmark.svg";
+import film from "../../public/assets/movies.svg";
+import tv from "../../public/assets/tvs.svg";
+
+const Movie = () => {
+  const Mydata = [...data];
+
+  return (
+    <Recommended>
+      <h1>Recommended for you</h1>
+      <div className="row">
+        {Mydata.map((data) => {
+          return (
+            data.isTrending === false && (
+              <div className="trending" key={data.title}>
+                <Video2 imgData={data.thumbnail.regular}>
+                  <div className="bookmark">
+                    <Image src={bookmark} alt="bookmark" />
+                  </div>
+                </Video2>
+                <div className="info">
+                  <div className="info-top">
+                    <h1>{data.year}</h1>
+                    <div className="category">
+                      {data.category === "Movie" ? (
+                        <Image src={film} alt="category" />
+                      ) : (
+                        <Image src={tv} alt="category" />
+                      )}
+
+                      <p>{data.category}</p>
+                    </div>
+                    <p>{data.rating}</p>
+                  </div>
+                  <div className="info-bottom">
+                    <h1>{data.title}</h1>
+                  </div>
+                </div>
+              </div>
+            )
+          );
+        })}
+      </div>
+    </Recommended>
+  );
+};
+
+const Recommended = styled.div`
+  margin-top: 2.4rem;
+  h1 {
+    font-family: "Outfit";
+    font-size: "2rem";
+    font-weight: 300;
+  }
+  .trending {
+    margin-top: 1.6rem;
+    margin-left: 1.6rem;
+  }
+
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .info {
+    font-family: "Outfit";
+
+    font-weight: 300;
+    .info-top {
+      display: flex;
+      align-items: center;
+      h1 {
+        font-size: 1.1rem;
+        font-weight: 300;
+      }
+      .category {
+        display: flex;
+        font-size: 1.1rem;
+        font-weight: 300;
+        margin-left: 2rem;
+        align-items: center;
+        p {
+          margin-top: 0;
+          margin-bottom: 0;
+          margin-left: 0.6rem;
+        }
+      }
+      p {
+        font-size: 1.1rem;
+        font-weight: 300;
+        margin-left: 2rem;
+        margin-top: 0;
+        margin-bottom: 0;
+      }
+    }
+    .info-bottom {
+      h1 {
+        margin: 0;
+        font-weight: 500;
+      }
+    }
+  }
+`;
+
+const Video2 = styled.div`
+  width: 17rem;
+  height: 11rem;
+  background: url(${(props) => props.imgData.small});
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 8px;
+  position: relative;
+  .bookmark {
+    background-color: #5a698f;
+    width: 3.2rem;
+    height: 3.2rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    right: 5%;
+    top: 5%;
+  }
+`;
+
+export default Movie;
---
+++ components/input/input.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import Search from "../../public/assets/icon-search.svg";
+import Image from "next/image";
+
+const InputCustom = ({ placeholder }) => {
+  return (
+    <InputWrap>
+      <button>
+        <Image src={Search} alt="search" />
+      </button>
+      <form>
+        <input type="text" placeholder={placeholder} />
+      </form>
+    </InputWrap>
+  );
+};
+
+const InputWrap = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 2.4rem;
+  button {
+    background-color: transparent;
+    cursor: pointer;
+    border: none;
+    outline: none;
+  }
+  form {
+    margin-left: 1.6rem;
+    input {
+      padding: 1rem 1.2rem;
+      width: 100%;
+      font-size: 1.6rem;
+      background-color: transparent;
+      outline: none;
+      border: none;
+      color: white;
+      width: 125%;
+      font-weight: 300;
+      font-family: "Outfit";
+      letter-spacing: 1px;
+    }
+  }
+`;
+
+export default InputCustom;
